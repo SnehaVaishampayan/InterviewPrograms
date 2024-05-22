4c6d74937e14db389e82ce6e3b82c2d0321c250e
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2024-05-22 13:06:58 -0700

    Time: 0 ms (100.00%) | Memory: 42 MB (33.87%) - LeetSync

---
+++ basic/array/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,42 @@
+// 3:50 - 4:05
+class Solution {
+    public int search(int[] nums, int target) {
+        // binary search
+        // get the mid..    if nums[mid] == target.. return mid
+        //                  if nums[start] < nums[mid] // left is sorted
+        //                         if (target between start and mid) .. binary search left
+        //                         else .. binary search right
+        //                  else if nums[mid] < nums[right] // right is sorted. 
+        //                        if (target between start and mid) .. binary search right
+        //                        else .. binary search left
+
+        int start = 0; int end = nums.length - 1; int mid = 0;
+        while(start <= end) {
+            mid = start + ( end - start)/2;
+            // System.out.println("mid " + mid);
+            // System.out.println("ele " + nums[mid]);
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[start] <= nums[mid]) {
+                if(nums[start] <= target && target <= nums[mid]) {
+                    end = mid-1;
+                }
+                else {
+                    start = mid+1;
+                }
+            }
+            else { //if(nums[mid] < nums[end] ) {
+                if(nums[mid] < target && target <= nums[end]) {
+                    // search right
+                    start = mid+1;
+                }
+                else {
+                    // search left
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
