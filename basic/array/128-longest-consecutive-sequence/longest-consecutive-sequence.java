f3da0c29e631f66e2a3c47cd11d8a84b97fbbb82
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2024-06-18 08:28:01 -0700

    Time: 1383 ms (5.15%) | Memory: 67.1 MB (23.34%) - LeetSync

--- basic/array/128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ basic/array/128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,58 +1,45 @@
 class Solution {
     // 7:25 - 7:35
     public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) return 0;
-        HashSet<Integer> set = new HashSet<>();
-        for (int n : nums){
-            set.add(n);
-         }
+    //    HashSet<Integer> hs = new HashSet<>();
 
-        int count = 1;
+    //     for (int i: nums){
+    //         hs.add(i);
+    //     }
+    //     int c =0;
 
-        for (int num : nums) {
-            if (!set.contains(num - 1)) {
-                int tempCount = 1;
-                while (set.contains(num + 1)) {
-                    tempCount++;
-                    num++;
-                }
-                count = Math.max(count, tempCount);
-            }
+    //     for (int x:hs){
+    //         if(!hs.contains(x-1)){
+    //             int y = x+1;
+    //             while(hs.contains(y)){
+    //                 y++;
+    //             }
+    //             c= Math.max(c,y-x);
+    //         }
+    //     }
+    //     return c;
+    // }
 
-            if (count > nums.length / 2) break;
-        }
-
-        return count;
-    }
-
-        // int count = 0; 
+        int res = 0; 
         // int res = 0; 
         // if(nums.length <= 0 || nums.length == 1) {
         //     return nums.length;
         // }        
         // // Add to the HashSet
-        // Set<Integer> hs = new HashSet<>();
-        // for(int i: nums) {
-        //     hs.add(i);
-        // }
+        Set<Integer> hs = new HashSet<>();
+        for(int i: nums) {
+            hs.add(i);
+        }
         // // Traverse through an array curr
-        // for(int currNum: nums) {
-        //     if(hs.contains(currNum - 1)) {
-        //         continue;
-        //     }
-        //     int currRight = currNum + 1;
-        //     if(hs.contains(currRight)) {
-        //         count = 1;
-                
-        //         while(hs.contains(currRight)) {
-        //             count++;
-        //             currRight++;
-        //         }
-        //         res = Math.max(count, res);
-        //     } else {
-        //         count = 1;
-        //     }
-        // }
-        // return res;
-    // }
+        for(int currNum: nums) {
+            if(!hs.contains(currNum - 1)) {
+                int currRight = currNum + 1;
+                while(hs.contains(currRight)) {
+                    currRight++;
+                }
+                res = Math.max(currRight - currNum, res);
+            }
+        }
+        return res;
+    }
 }
