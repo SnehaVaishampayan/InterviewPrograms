b290140366619e059775aa14e20f78f31a9b5066
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2024-07-16 12:18:54 -0700

    Time: 1 ms (83.17%) | Memory: 41.2 MB (69.86%) - LeetSync

---
+++ basic/array/290-word-pattern/word-pattern.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if(words.length != pattern.length()) {
+            return false;
+        }
+        Map<String, Character> hm = new HashMap<>();
+        // int chCount = 0;
+        // System.out.println((char) (chCount + 'a'));
+        int patternItr = 0;
+        for(String eachWord:  words){
+            if(patternItr >= pattern.length()) {
+                return false;
+            }
+            if(!hm.containsKey(eachWord)) {
+                if(hm.values().contains(pattern.charAt(patternItr)) ) {
+                    return false;
+                }
+                hm.put(eachWord, pattern.charAt(patternItr));                
+            }
+            else {
+                // check if char matches
+                if(pattern.charAt(patternItr) != hm.get(eachWord)){
+                    return false;
+                }
+            }
+            patternItr++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String eachWord:  words){
+            sb.append(hm.get(eachWord));
+        }
+        
+        // return sb.toString().equals(pattern);
+        return true;
+    }
+}
