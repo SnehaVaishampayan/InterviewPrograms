0de4d0e7766dd6e910a44b0f840084e4b32442e9
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2024-07-17 15:55:44 -0700

    Time: 338 ms (5.31%) | Memory: 58.7 MB (8.22%) - LeetSync

--- basic/array/209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ basic/array/209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -5,29 +5,23 @@ class Solution {
         //     if == target.. calc resultLen 
         //     if currSum > target.. move start
 
-        int start  = 0;
+        int start = 0;
         int end = 0;
         int resultLen = Integer.MAX_VALUE;
-        int currSum = 0;//nums[0];
+        int currSum = 0;
         while(end < nums.length) {    //  
             System.out.println("currSum " + currSum);
             currSum = currSum + nums[end]; 
             end++;
-            // if(nums[end] == target || nums[start] == target) {
-            //     return 1;
-            // }
-            // else if( currSum >= target) {
             while(currSum >= target) {
                 // calc result.. 
-                // resultLen = resultLen == 0 ? end-start : Math.min(resultLen, end-start+1);
                 resultLen = Math.min(resultLen, end-start);
                 currSum = currSum - nums[start];
                 start++;
-                // end = start;
             }
         }
         System.out.println("start " + start);
         System.out.println("end " + end);
-        return resultLen  == Integer.MAX_VALUE ? 0 :resultLen ;//Math.min(resultLen, end-start+1);
+        return resultLen  == Integer.MAX_VALUE ? 0 : resultLen;
     }
 }
