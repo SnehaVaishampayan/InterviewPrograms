c1c779d499fa5de8c19cb449f0b29e19f46298a1
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2024-07-23 13:08:47 -0700

    Time: 7 ms (80.62%) | Memory: 44.2 MB (69.18%) - LeetSync

--- basic/array/340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.java
+++ basic/array/340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.java
@@ -19,61 +19,74 @@ class Solution {
         //  startCharLastOccurence= 2
         // start = startCharLastOccurence + 1 = 3 
 
-        // if(s == null || s.length() == 0 ){
-        //     return 0;
-        // } 
-        // if(k >= s.length()) {
-        //     return s.length();
-        // }
-        // Map<Character, Integer> hm = new HashMap<>();
-        // int start = 0;
-        // int result = k;
-        // for(int end = 0; end < s.length(); end++) {
-        //     char currChar = s.charAt(end);
-        //     hm.put(currChar, end);
-        //     if( hm.size() > k) {
-        //         char startChar = s.charAt(start);
-        //         System.out.println("start " + start);
-        //         System.out.println("end " + end);
-
-        //         result = Math.max(result, end-start+1);
-        //         System.out.println("result " + result);
-        //         start++;// = hm.get(startChar) + 1;
-        //      }
-        // }
-        // if(hm.size() <= k)
-        //     return s.length();
-        // // result = Math.max(result, s.length()-start+1);
-        // return result;
-
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        if(k==0 || s==null || s.length()==0)
+        if(s == null || s.length() == 0 ){
             return 0;
-        if(s.length()<k)
+        } 
+        if(k >= s.length()) {
             return s.length();
-        int maxLen=k;
-        int left = 0;
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(map.containsKey(c)){
-                map.put(c, map.get(c)+1);
-            }else{
-                map.put(c, 1);
-            }
-            if(map.size()>k){
-                maxLen=Math.max(maxLen, i-left);
-                while(map.size()>k) {
-                    char fc = s.charAt(left);
-                    if(map.get(fc)==1){
-                        map.remove(fc);
+        }
+        Map<Character, Integer> hm = new HashMap<>();
+        int start = 0;
+        int result = k;
+        for(int end = 0; end < s.length(); end++) {
+            char currChar = s.charAt(end);
+
+            hm.put(currChar, hm.getOrDefault(currChar,0) + 1);
+
+            if( hm.size() > k) {
+                
+                result = Math.max(result, end-start);
+                while(hm.size() > k ) {
+                    char startChar = s.charAt(start);
+                    if(hm.get(startChar) == 1) {
+                        hm.remove(startChar);
                     }
                     else {
-                    map.put(fc, map.get(fc)-1);
+                        hm.put(startChar, hm.get(startChar) - 1);
                     }
-                left++;
-                }        
-            }
+                    start++;
+                }                
+             }
         }
-        return Math.max(maxLen, s.length()-left);
+        return Math.max(result, s.length()-start);
     }
+        // // HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        // // if(k == 0 || s == null || s.length()==0)
+        // //     return 0;
+
+        // // if(s.length()<k)
+        // //     return s.length();
+
+        // int result = k;
+        // int start = 0;
+        // for(int end = 0; end < s.length(); end++) {
+        //      char currChar = s.charAt(end);
+
+        //     // if(map.containsKey(currChar)){
+        //     //     map.put(currChar, map.get(currChar) + 1);
+        //     // }else{
+        //     //     map.put(currChar, 1);
+        //     // }
+        //     map.put(currChar, end);
+
+
+        //     if(map.size() > k){
+
+        //         result = Math.max(result, end - start);
+        //         start = map.get(s.charAt(start)) + 1;
+        //         // while(map.size() > k) {
+        //             // char fc = s.charAt(start);
+        //             // if(map.get(fc)==1){
+        //             //     map.remove(fc);
+        //             // }
+        //             // else {
+        //             // map.put(fc, map.get(fc)-1);
+        //             // }
+                    
+        //         // start++;
+        //         // }        
+        //     }
+        // }
+        // return Math.max(result, s.length()-start);
+    // }
 }
