c7a9342c09e78b14dda169e197b2a0c3766dcf48
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2024-09-19 11:16:31 -0700

    Time: 1 ms (98.59%) | Memory: 41.5 MB (52.73%) - LeetSync

--- basic/array/20-valid-parentheses/valid-parentheses.java
+++ basic/array/20-valid-parentheses/valid-parentheses.java
@@ -1,40 +1,36 @@
 class Solution {
     public boolean isValid(String s) {
-        // if open put in stack.. 
-        // if close pop and match
-        if(s == null || s.length() <= 0) {
-            return false;
-        }
+        // if open put in stack. 
+        // if close.. match with top
 
-        String openBrackets = "([{";
+        // Valdiations
+        if(s == null || s.length() == 0) {
+            return true;
+        }
+        
         Stack<Character> st = new Stack<>();
-        for(int i = 0;i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(openBrackets.indexOf(c) != -1) {
+        String openBrackets = "([{";
+        for(char c: s.toCharArray()) {
+            if(openBrackets.indexOf(c) >= 0) {
+                // it is open .. put in stack
                 st.push(c);
             }
             else {
-                if(st.isEmpty()) {
+                // it is close
+                if(st.isEmpty()){
                     return false;
                 }
                 char top = st.pop();
-                if(c == ')' ){
-                    if(top != '(') {
-                        return false;
-                    }
+                if(top == '(' && c != ')')  {
+                    return false;
                 }
-                if(c == ']') {
-                    if(top != '[') {
-                        return false;
-                    }
+                else if(top == '{' && c != '}') {
+                    return false;
                 }
-                if(c== '}') {
-                    if(top != '{') {
-                        return false;
-                    }
-                }
+                
             }
         }
         return st.isEmpty();
+
     }
 }
