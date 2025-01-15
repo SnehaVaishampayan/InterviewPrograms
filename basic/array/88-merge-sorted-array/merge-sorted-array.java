c0b0a3e4c44e8b5f1184a8c07577c7bda2b42228
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2025-01-15 10:20:07 -0800

    Time: 0 ms (100.00%) | Memory: 42.1 MB (83.80%) - LeetSync

--- basic/array/88-merge-sorted-array/merge-sorted-array.java
+++ basic/array/88-merge-sorted-array/merge-sorted-array.java
@@ -1,28 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // null checks
+        // Work backwards
+        int resultItr = m + n-1;
+        int n2ItrFromLast = n-1;
+        int n1ItrFromLast = m-1;
 
-        // iterated p1 = m-1, p2 =n-1
-        //      bigger element .. put in nums1
-
-        // if(nums1 == null || nums2 == null) {
-        //     return nums1;
-        // }
-        
-        int p1 = m-1; int p2 = n-1;
-        int resItr = nums1.length-1;
-
-        while(resItr >= 0 && p2 >= 0) {
-            System.out.println("p1 " + p1 + " p2 " + p2);
-            if(p1 >= 0 && p2 >= 0 && nums1[p1] > nums2[p2]) {
-                nums1[resItr] = nums1[p1]; 
-                p1--;               
+        while(n1ItrFromLast >= 0 && n2ItrFromLast >= 0) {
+            // see which one is big elelment
+            if(nums1[n1ItrFromLast] > nums2[n2ItrFromLast] ) {
+                nums1[resultItr] = nums1[n1ItrFromLast];
+                n1ItrFromLast--;
             }
-            else {
-                nums1[resItr] = nums2[p2];
-                p2--;
+            else{
+                nums1[resultItr] = nums2[n2ItrFromLast];
+                n2ItrFromLast--;
             }
-            resItr--;
+            resultItr--;
+        }
+        while(n2ItrFromLast >= 0 ) {
+            nums1[resultItr] = nums2[n2ItrFromLast];
+            n2ItrFromLast--;
+            resultItr--;
         }
     }
 }
