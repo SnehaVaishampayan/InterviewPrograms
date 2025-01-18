489eb7b99603ce271ca3871f4e3402488e7a0e26
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2025-01-17 22:54:00 -0800

    Time: 5 ms (3.99%) | Memory: 41.6 MB (35.89%) - LeetSync

---
+++ basic/array/392-is-subsequence/is-subsequence.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        // traverse s .. 
+        //      traverse t and find firstChar.. 
+        //          match next chars .. 
+        //              if all found.. return true.. 
+        //          
+        if(s == null && t == null) {
+            return true;
+        }
+        if(t.length() < s.length()) {
+            return false;
+        }
+        if(s.length() == 0 ) {
+            return true;
+        }
+        int sItr = 0;
+        char s_match = s.charAt(sItr);
+        for(int i = 0 ; i < t.length(); i++) {
+
+            if(t.charAt(i) == s_match) {
+
+                System.out.println("sItr " + sItr);
+                System.out.println("s_match " + s_match);
+                if(sItr < s.length()-1) {
+                    sItr++;
+                    s_match = s.charAt(sItr);
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+        return sItr >= s.length();
+    }
+    // private boolean matchNextChars(String s, String t, int sItr, int tItr) {
+    //     while(s.charAt(sItr) == t.charAt(tItr)) {
+    //         sItr++;
+    //         tItr++;
+    //     }
+    //     return sItr >= s.length();
+    // }
+}
