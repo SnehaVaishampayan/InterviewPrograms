5e0380895eb3b0857c0e3eb778abe3bc8bef676a
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2025-01-21 08:18:47 -0800

    Time: 7 ms (64.50%) | Memory: 48 MB (54.17%) - LeetSync

--- basic/array/49-group-anagrams/group-anagrams.java
+++ basic/array/49-group-anagrams/group-anagrams.java
@@ -1,28 +1,85 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // sort str 
-        // map .. sorted string and word
-        
-        Map<String, List<String>> hm = new HashMap<>();
+        // HashMap sortedStrs 
+        // Traverse strs 
+        //      
         List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> hm = new HashMap<>();
         for(String s: strs) {
+            // Checks 
+            if(s == null) {
+                continue;
+            }
+            // Sort char array of input s
             char[] ch = s.toCharArray();
             Arrays.sort(ch);
             String sortedStr = new String(ch);
-            List<String> values;
+            List<String> al;
             if(hm.containsKey(sortedStr)) {
-                 values = hm.get(sortedStr);      
-            }
+                al = hm.get(sortedStr);
+                al.add(s);                
+            }   
             else {
-                 values = new ArrayList<>();
+                al = new ArrayList<>();
+                al.add(s);
             }
-            values.add(s);
-            hm.put(sortedStr, values);            
+            hm.put(sortedStr, al);
         }
+
         for(Map.Entry<String, List<String>> e: hm.entrySet()) {
             result.add(e.getValue());
-            System.out.println(e.getValue());
         }
-        return result;
+        return result; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // // sort str 
+        // // map .. sorted string and word
+        
+        // Map<String, List<String>> hm = new HashMap<>();
+        // List<List<String>> result = new ArrayList<>();
+        // for(String s: strs) {
+        //     char[] ch = s.toCharArray();
+        //     Arrays.sort(ch);
+        //     String sortedStr = new String(ch);
+        //     List<String> values;
+        //     if(hm.containsKey(sortedStr)) {
+        //          values = hm.get(sortedStr);      
+        //     }
+        //     else {
+        //          values = new ArrayList<>();
+        //     }
+        //     values.add(s);
+        //     hm.put(sortedStr, values);            
+        // }
+        // for(Map.Entry<String, List<String>> e: hm.entrySet()) {
+        //     result.add(e.getValue());
+        //     System.out.println(e.getValue());
+        // }
+        // return result;
     }
 }
