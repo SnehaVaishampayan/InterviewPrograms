273e75ad771ee84a822b3fc9af32798da54627a0
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2025-01-22 10:38:01 -0800

    Time: 0 ms (100.00%) | Memory: 42 MB (29.36%) - LeetSync

--- basic/array/101-symmetric-tree/symmetric-tree.java
+++ basic/array/101-symmetric-tree/symmetric-tree.java
@@ -18,10 +18,10 @@ class Solution {
         if(root == null ) {
             return true;
         }
-        return isSameTree(root.left, root.right);
+        return isMirrorImageTree(root.left, root.right);
     }
 
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    public boolean isMirrorImageTree(TreeNode p, TreeNode q) {
         if(p == null && q == null) {
             return true;
         }
@@ -31,6 +31,6 @@ class Solution {
         if(p.val != q.val) {
             return false;
         }
-        return isSameTree(p.left, q.right) && isSameTree(p.right, q.left);
+        return isMirrorImageTree(p.left, q.right) && isMirrorImageTree(p.right, q.left);
     }
 }
