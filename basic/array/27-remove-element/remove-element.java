e7bc22aa884313ee82f0afe3419f821857c7048d
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2025-01-21 18:14:00 -0800

    Time: 0 ms (100.00%) | Memory: 42.4 MB (8.65%) - LeetSync

--- basic/array/27-remove-element/remove-element.java
+++ basic/array/27-remove-element/remove-element.java
@@ -1,11 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+// TEMPLATE : POINTERS - i (currElePtr) result (resultPtr)
+
         // return num of ele not equal to val
         int result = 0;
-        
-        for(int i: nums) {
-            if(i != val) {
-                nums[result] = i;
+        for(int currEle : nums) {
+            if(currEle != val) {
+                nums[result] = currEle;
                 result++;
             }
         }
