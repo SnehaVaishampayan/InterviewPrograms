e3d84056088c12d96550387439cad58a1ad0eca6
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2025-01-21 18:06:20 -0800

    Time: 0 ms (100.00%) | Memory: 45 MB (38.25%) - LeetSync

--- basic/array/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ basic/array/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,5 +1,8 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+
+// TEMPLATE : POINTERS - resultItr / itr
+
         //  Traverse array
         //      selectedEle .. 
         //      resItr == selectedEle
@@ -9,23 +12,37 @@ class Solution {
         if(nums.length <= 1) {
             return nums.length;
         }
-        int resultItr = 1;
-        int itr = 1;
-        int selectedEle = nums[0];
+        // int resultItr = 1;
+        // int runner = 1;
+        // int selectedEle = nums[0];
         
-        while(itr < nums.length) {
-            while(itr < nums.length && nums[itr] == selectedEle) {
-                itr++;
+        // while(runner < nums.length) {
+        //     while(runner < nums.length && nums[runner] == selectedEle) {
+        //         itr++;
+        //     }
+        //     if(runner >= nums.length ) {
+        //         return resultItr;
+        //     }
+        //     else{                
+        //         nums[resultItr] = nums[runner];
+        //         selectedEle = nums[resultItr];
+        //         resultItr++;
+        //     }
+        // }
+        // return resultItr;
+    
+
+
+        int runner = 1;
+        int resPtr = 1;
+
+        while(runner < nums.length) {            
+            if( nums[runner] != nums[resPtr-1]) {   // if currEle is not equal to last 2 ele of result arrray           
+                nums[resPtr] = nums[runner];
+                resPtr++;
             }
-            if(itr >= nums.length ) {
-                return resultItr;
-            }
-            else{                
-                nums[resultItr] = nums[itr];
-                selectedEle = nums[resultItr];
-                resultItr++;
-            }
+            runner++;
         }
-        return resultItr;
+        return resPtr;
     }
 }
