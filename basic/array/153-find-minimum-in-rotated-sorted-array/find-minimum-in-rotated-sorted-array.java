197e6d0de97c8bb74ed62b17e0304bbf445a7eae
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2025-01-22 09:35:16 -0800

    Time: 0 ms (100.00%) | Memory: 42 MB (71.69%) - LeetSync

--- basic/array/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ basic/array/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,27 +1,45 @@
 class Solution {
     public int findMin(int[] nums) {
+        // TEMPLATE Binary Search
+
+        // Recursively 
+        //      find mid
+        //      for mid .. check if it is min by comparing with sides.. 
+        //      if not move to unsorted part
+        //      if start < end .. means sorted.. 
+        //      else .. search here 
+
+
+        if(nums == null || nums.length == 0) {
+            return -1;
+        } 
+        if( nums.length == 1) {
+            return nums[0];
+        }
+        if(nums.length == 2) {
+            return nums[0] < nums[1] ? nums[0] : nums[1];
+        }
+
         int start = 0; int end = nums.length-1;
-        if(nums[start] < nums[end]) {
+
+        if(nums[start] <= nums[end]) {
             return nums[start];
         }
-        while(start <= end) {
 
-            if((end - start) == 1) {
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if( (end-start) == 1) {         // While terminating condition
                 return nums[end];
             }
-            int mid = start + (end - start)/2;
-            if(nums[start] > nums[mid]) {
-                
-                end = mid;      // Move to Unsorted left 
-            }
-            else if (nums[mid] > nums[end]) {
-                start = mid;
+
+            if( nums[start] >= nums[mid]){      // UNSorted Array
+                end = mid;
             }
             else {
-                return nums[start];// Move to Unsorted left 
-                }
+                start = mid;    
+            }
         }
-    
-        return nums[start];
+        return -1;
     }
 }
