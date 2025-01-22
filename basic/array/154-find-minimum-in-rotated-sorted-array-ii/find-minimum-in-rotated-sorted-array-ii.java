25f914807670a018f15897c0f533c74a073e6c4b
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2025-01-22 10:02:17 -0800

    Time: 0 ms (100.00%) | Memory: 43.2 MB (42.78%) - LeetSync

---
+++ basic/array/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int findMin(int[] nums) {
+                // TEMPLATE Binary Search
+
+        // Recursively while start <= end
+        //      find mid
+        //      if end-start == 1 .. while termination.. return end
+        //      Look up to unsorted part
+        //      if start > end .. means unsorted.. end = mid
+        //      else .. search here .. start = mid
+
+
+        if(nums == null || nums.length == 0) {
+            return -1;
+        } 
+        if( nums.length == 1) {
+            return nums[0];
+        }
+        if(nums.length == 2) {
+            return nums[0] < nums[1] ? nums[0] : nums[1];
+        }
+
+        int start = 0; int end = nums.length-1;
+
+
+
+        while(start <= end) {
+            while((nums[start] == nums[end]) && (start != end)) {
+                start++;
+            }
+            
+            if(nums[start] <= nums[end]) {
+                return nums[start];
+            }
+
+            int mid = start + (end - start) / 2;
+            if( nums[start] > nums[mid]){      // UNSorted Array
+                end = mid;
+            }
+            else {
+                start = mid+1;    
+            }
+        }
+        return -1;
+    }
+}
