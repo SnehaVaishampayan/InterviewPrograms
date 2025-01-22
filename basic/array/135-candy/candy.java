6ad6afc40ffc81e6a424fbb071fad9b00f6369e7
Sneha Vaishampayan <sneha_gadre@yahoo.com>
2025-01-21 18:37:24 -0800

    Time: 4 ms (28.40%) | Memory: 46.1 MB (17.28%) - LeetSync

---
+++ basic/array/135-candy/candy.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int candy(int[] ratings) {
+        // Traverse the array from left to right
+        //      Assign values for all the ascending pairs
+        // Traverse right to left
+        //      Assign values to descending pairs.
+
+        if(ratings == null || ratings.length == 0) {
+            return 0;
+        }
+        int[] candies = new int[ratings.length];
+        candies[0] = 1; 
+
+        for(int i = 1; i < ratings.length; i++) {
+            if(ratings[i-1] < ratings[i]) {     // Ascending order
+                candies[i] = candies[i-1] + 1;
+            }
+            else {
+                candies[i] = 1;
+            }
+        }
+        int result = candies[candies.length - 1];
+        for(int i = ratings.length - 2; i >= 0; i-- ) {
+            int newCandies = 1; 
+
+            if(ratings[i] > ratings[i+1]) {     // Descending order
+                newCandies = candies[i+1] + 1;
+            }
+            result += Math.max(newCandies, candies[i]);
+            candies[i] = newCandies;
+        }
+        return result;
+    }
+}
